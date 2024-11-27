d7dfecd6e119ef51d4b1c83ff9c7256d039b97ba
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-11-27 14:30:06 +0700

    add comment

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 //     println!("Hello, world!");
 // }
 
+// note ! std = standart library rust
+// :: operator ini pemisah sepertinya
+// io adalah module yg ada di standard library rust
 use std::io;
 
 fn main() {
@@ -9,11 +12,17 @@ fn main() {
 
     println!("Please input your guess.");
 
+    // example
+    // let guess = String::new() // ini immutable => ini mirip const yang tidak bisa diubah di js
+    // let guess = String::new() // ini mutable => ini mirip let yang ada di js yang bisa di ubah
+
     let mut guess = String::new();
 
+    // sedangkan stdin adalah function yang ada pada io module (modul io)
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You Guess : {}", guess);
+
 }

afdd2e6248c3d7161a9f57d3007e124af8b708b6
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-12-22 19:43:12 +0700

    add variables

---
+++ variables/target/debug/deps/variables-6574392f93502a8f.d
@@ -0,0 +1,9 @@
+/home/rahman/Rahman/github/LearnRust/variables/target/debug/deps/libvariables-6574392f93502a8f.rmeta: src/main.rs Cargo.toml
+
+/home/rahman/Rahman/github/LearnRust/variables/target/debug/deps/variables-6574392f93502a8f.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR

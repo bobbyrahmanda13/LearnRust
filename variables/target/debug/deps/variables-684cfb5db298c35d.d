fea15911732bfaab102b3454abcef5d0adbac794
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-12-22 19:44:21 +0700

    remove target variables

--- variables/target/debug/deps/variables-684cfb5db298c35d.d
+++
@@ -1,9 +0,0 @@
-/home/rahman/Rahman/github/LearnRust/variables/target/debug/deps/libvariables-684cfb5db298c35d.rmeta: src/main.rs Cargo.toml
-
-/home/rahman/Rahman/github/LearnRust/variables/target/debug/deps/variables-684cfb5db298c35d.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR

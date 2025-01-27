636788d04564f462d4e5c9665f45bd8b0538f745
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-01-27 20:17:52 +0700

    add attribute test rust

--- udemy-rust/belajar-rust-dasar/src/main.rs
+++ udemy-rust/belajar-rust-dasar/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    // note :
+    // menggunakan print!(text) = ini jika digunakan akan menulis tapi tanpa di enter jadi
+    // hasilnya akan gabung
+    // jika menggunakan println!(text) = ini jika digunakan akan menulis tapi nanti di enter dan hasil nya tidak akan di gabung
     println!("Hello, world!");
+    print!("Hello, world!");
+    print!("Hello, world!");
+}
+
+#[test]
+fn hello_test() {
+    println!("hello test")
 }
